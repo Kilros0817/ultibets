d0903de463a92e2b03ff9301915ed073569421d3
Kilros0817 <lazarkitfrl@gmail.com>
2023-08-13 16:02:12 +0800

    update mumbai rpc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -28,7 +28,7 @@ const polygonMumbai = {
     symbol: 'MATIC',
   },
   rpcUrls: {
-    default: { http: ['https://rpc-mumbai.maticvigil.com/'] },
+    default: { http: ['https://rpc.ankr.com/polygon_mumbai'] },
   },
   blockExplorers: {
     etherscan: { name: 'PolygonScan', url: 'https://mumbai.polygonscan.com/' },
