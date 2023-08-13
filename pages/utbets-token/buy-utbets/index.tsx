23e70decf9a8931c373893673b107ea3ba5e9614
Kilros0817 <lazarkitfrl@gmail.com>
2023-08-13 15:51:26 +0800

    refine buy token page

--- pages/utbets-token/buy-utbets/index.tsx
+++ pages/utbets-token/buy-utbets/index.tsx
@@ -1,40 +1,11 @@
 import {
-	Box,
 	Flex,
-	Image,
-	Switch,
-	Text,
-	Menu,
-	MenuButton,
-	MenuList,
-	MenuItem,
-	Button,
-	useDisclosure,
-	Input,
 } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import UtBetsTokenRoutes from "../tokenRoutes";
 import {
-	mumbaiChainId,
-	newChainAttrs,
-	polygonChainId,
 	uniswapURL,
-	utbetsTokenAddresses,
 } from '../../../utils/config';
-import { useAccount, useBalance, useNetwork } from 'wagmi';
-import {
-	useGetAmountsOut,
-	useSwapExactETHForTokens,
-	useSwapExactTokensForETH,
-	useSwapExactTokensForTokens,
-	useApprove,
-} from '../../../utils/interact/sc/utbets';
-import { checkIconInGreenBg } from '../../../utils/assets';
-import AnnounceModal from '../../../components/modal/AnnounceModal';
-import { ethers } from 'ethers';
-import { toast } from 'react-toastify';
-import { checkIfWalletConnected } from '../../../utils/interact/utility';
 
 const BuyUtbets = () => {
 	return (
