8e47fc251964fae925a1687c9897ab34bf81bdff
Kilros0817 <lazarkitfrl@gmail.com>
2023-09-05 16:24:53 +0800

    update shipping price

--- constant/continents.ts
+++ constant/continents.ts
@@ -19,29 +19,29 @@ export const continents = [
 export const shippingCost = [
   {
     continent: 'Africa',
-    price: 30,
+    price: 45,
   },
   {
     continent: 'Asia',
-    price: 30,
+    price: 45,
   },
   {
     continent: 'Europe',
-    price: 20,
-  },
-  {
-    continent: 'America',
     price: 30,
   },
   {
+    continent: 'America',
+    price: 45,
+  },
+  {
     continent: 'Oceania',
-    price: 40,
+    price: 60,
   },
 ];
 
-// Africa =>  1kg - 30 USDC
-// North America =>  1kg - 30 USDC
-// South America =>  1kg - 30 USDC
-// Asia =>  1kg - 30 USDC
-// Europe =>  1kg - 20 USDC
-// Oceania =>  1kg - 40 USDC
+// Africa =>  1kg - 45 USDC
+// North America =>  1kg - 45 USDC
+// South America =>  1kg - 45 USDC
+// Asia =>  1kg - 45 USDC
+// Europe =>  1kg - 30 USDC
+// Oceania =>  1kg - 60 USDC
