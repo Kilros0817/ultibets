92fb46143994c14cf74490eb2722a009f864a2f2
Kilros0817 <lazarkitfrl@gmail.com>
2023-09-08 16:41:13 +0800

    add chain

--- utils/networks.ts
+++ utils/networks.ts
@@ -43,3 +43,47 @@ export const polygonMumbai = {
  },
   testnet: true,
 } as Chain;
+
+export const arbitrumGoerli = {
+    id: 421613,
+    name: "Arbitrum Goerli",
+    network: "arbitrum-goerli",
+    nativeCurrency: {
+        name: "Arbitrum Goerli Ether",
+        symbol: "ETH",
+        decimals: 18,
+    },
+    rpcUrls: {
+        alchemy: {
+            http: ["https://arb-goerli.g.alchemy.com/v2"],
+            webSocket: ["wss://arb-goerli.g.alchemy.com/v2"],
+        },
+        infura: {
+            http: ["https://arbitrum-goerli.infura.io/v3"],
+            webSocket: ["wss://arbitrum-goerli.infura.io/ws/v3"],
+        },
+        default: {
+            http: ["https://goerli-rollup.arbitrum.io/rpc"],
+        },
+        public: {
+            http: ["https://arb-goerli.g.alchemy.com/v2/btl1mNqjNlVRsa6SDM-44fZSL_Fx60w9"],
+        },
+    },
+    blockExplorers: {
+        etherscan: {
+            name: "Arbiscan",
+            url: "https://goerli.arbiscan.io/",
+        },
+        default: {
+            name: "Arbiscan",
+            url: "https://goerli.arbiscan.io/",
+        },
+    },
+    contracts: {
+        multicall3: {
+            address: "0xca11bde05977b3631167028862be2a173976ca11",
+            blockCreated: 88114,
+        },
+    },
+    testnet: true,
+} as Chain;
