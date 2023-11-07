5721ce38998a6469a9bbf0f07ec7223d8847c1f9
Kilros0817 <lazarkitfrl@gmail.com>
2023-11-07 16:14:53 +0800

    secure api request

--- lib/iron.ts
+++ lib/iron.ts
@@ -3,7 +3,7 @@ import { IronSessionOptions } from 'iron-session';
 export const ironOptions: IronSessionOptions = {
   cookieName: 'Ultibets',
   cookieOptions: {
-    secure: false,
+    secure: true,
   },
   password: process.env.NEXT_IRON_PASSWORD as string,
 };
